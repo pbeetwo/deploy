a35a1ef0de402f927ad08359ed859092e17221b9
pbeetwo <monkeyscience@gmail.com>
2017-08-10 00:09:58 +0200

    First commit

---
+++ gsettings.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Script to configure settings through gsettings for Ubuntu16.04 Unity.
+/usr/bin/gsettings set com.canonical.Unity.Launcher launcher-position Left
+/usr/bin/gsettings set com.canonical.indicator.session show-real-name-on-panel true
