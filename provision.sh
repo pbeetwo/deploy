246f5a79c15906c870e65681e3f1635d6213f01c
root <lol@lol.com>
2017-08-19 21:07:06 +0200

    LOl

--- provision.sh
+++ provision.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+# Install latest version of Ansible if not installed and execute playbook deploywork.yml
+
+if test -e /usr/bin/ansible ; then
+	echo "Ansible already installed"
+else
+	apt-get install software-properties-common -y
+        apt-add-repository ppa:ansible/ansible -y
+        apt-get update
+        apt-get install ansible -y
+fi
+
 /usr/bin/ansible-playbook deploywork.yml
